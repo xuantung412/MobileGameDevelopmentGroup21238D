9a9fccf28c0c4ea17acb5c8629401d52c17645e8
Tung NGUYEN <Nguyen@Trans-MacBook-Pro.local>
2017-03-10 14:12:38 +1030

    Initial commit

---
+++ app/src/main/java/unisa/nguty218/mobilegamedevelopmentgroup21238d/MainActivity.java
@@ -0,0 +1,13 @@
+package unisa.nguty218.mobilegamedevelopmentgroup21238d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
